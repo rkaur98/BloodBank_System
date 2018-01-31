b843044b4169256ac84a4372931b8b90162f9abf
rkaur98 <rupinderbal.rupi@gmail.com>
2018-01-31 15:22:54 -0800

    Update connect.php

--- Bloodbank/connect.php
+++ Bloodbank/connect.php
@@ -1,11 +1,11 @@
 <?php
 
 // Create connection
-// $conn = mysqli_connect("localhost", "root", "", "Bloodbank");
+$conn = mysqli_connect("nutritionbox.database.windows.net", "rupkaur98", "rk@6904$", "NutritionBox");
 
 // Check connection
-//if (!$conn) {
-//    die("Connection failed: " . mysqli_connect_error());
-// }
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>
