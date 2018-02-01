315eab56eea6d448c9a29292aafc8bf74eb81116
rkaur98 <rupinderbal.rupi@gmail.com>
2018-01-31 19:18:43 -0800

    Update connect.php

--- Bloodbank/connect.php
+++ Bloodbank/connect.php
@@ -41,7 +41,7 @@ $db_name = 'NutritionBox';
 
 //Establishes the connection
 $conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 1433);
+mysqli_real_connect($conn, $host, $username, $password, $db_name);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
