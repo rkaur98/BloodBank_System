ec93d8798a8c2848e881f20adf93c6561589674c
rkaur98 <rupinderbal.rupi@gmail.com>
2018-01-31 20:17:49 -0800

    Update connect.php

--- Bloodbank/connect.php
+++ Bloodbank/connect.php
@@ -14,7 +14,7 @@ echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
- echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+ echo ($row['B_Name'] . " " . $row['BNO'] . PHP_EOL);
 }
 sqlsrv_free_stmt($getResults);
 ?>
